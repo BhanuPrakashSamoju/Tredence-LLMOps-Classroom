698d2dbace9a8ad0d2c4b808994ec8d9f8174fd8
Bhanu.Prakash <bhanu.prakash@us.nestle.com>
2024-09-16 16:33:37 +0530

    Version 1 RAG Setup with Phoenix Tracer

--- assignments/assignment_03/rag_chatbot/app.py
+++ assignments/assignment_03/rag_chatbot/app.py
@@ -1,154 +1,300 @@
 import chainlit as cl
-from chainlit.types import AskFileResponse
+import pandas as pd
 import os
-# import phoenix as px
-# from phoenix.trace.langchain import OpenInferenceTracer, LangChainInstrumentor
+import validators
+import phoenix as px
+from phoenix.trace.langchain import LangChainInstrumentor
+from phoenix.otel import register
 
-from typing import List
-
-from langchain_community.document_loaders import PyPDFLoader
+import bs4
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.schema import (
-    HumanMessage,
-    AIMessage
-    # SystemMessage,
+from langchain.chains import (
+    create_history_aware_retriever, 
+    create_retrieval_chain
 )
-from langchain_community.chat_models.huggingface import ChatHuggingFace
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-
-from langchain_core.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
-)
-
-# from langchain.embeddings.openai import OpenAIEmbeddings
-
-# from langchain.vectorstores import Chroma
-# from langchain.chains import (
-#     ConversationalRetrievalChain,
-# )
-# from langchain.chat_models import ChatOpenAI
-
-from chainlit.types import AskFileResponse
-
-system_template = """Use the following pieces of context to answer the users question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
-The "SOURCES" part should be a reference to the source of the document from which you got your answer.
-And if the user greets with greetings like Hi, hello, How are you, etc reply accordingly as well.
-Example of your response should be:
-The answer is foo
-SOURCES: xyz
-Begin!
-----------------
-{summaries}"""
-messages = [
-    SystemMessagePromptTemplate.from_template(system_template),
-    HumanMessagePromptTemplate.from_template("{question}"),
-]
-prompt = ChatPromptTemplate.from_messages(messages)
-chain_type_kwargs = {"prompt": prompt}
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain.schema.runnable.config import RunnableConfig
+from langchain.callbacks.base import BaseCallbackHandler
 
 
-def process_file(file: AskFileResponse):
-    import tempfile
+def process_url_content(url):
+    # Load, chunk and index the contents of the blog.
+    loader = WebBaseLoader(
+        web_paths=(url,),
+    )
+    loader.requests_kwargs = {'verify': False}
+    docs = loader.load()
+    # Extract the page content
+    page_content = docs[0].page_content
 
-    with tempfile.NamedTemporaryFile(mode="w", delete=False) as tempfile:
-        with open(tempfile.name, "wb") as f:
-            f.write(file.content)
+    # Parse the content with BeautifulSoup
+    soup = bs4.BeautifulSoup(page_content, 'html.parser')
 
-    pypdf_loader = PyPDFLoader(tempfile.name)
-    texts = pypdf_loader.load_and_split()
-    texts = [text.page_content for text in texts]
-    return texts
+    # Extract text from the parsed HTML
+    text = soup.get_text()
+
+    doc = Document(
+        page_content=text,
+        metadata=docs[0].metadata,
+        id=1,
+    )
+
+    return [doc]
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    store = cl.user_session.get("store", {})
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+async def validate_url(url):
+    if not validators.url(url):
+        await cl.Message(content="Invalid URL. Please try again.").send()
+        return False
+    else:
+        return True
+    
+    
+async def set_up_llm_chain():
+    
 
 
 @cl.on_chat_start
 async def on_chat_start():
-    files = None
+
+    # print(f"Hugging Face Token Read: {os.environ.get("HUGGINGFACEHUB_API_TOKEN")}")
+
+    px_session = px.launch_app()
+
+    tracer_provider = register(
+        project_name="llm-classroom-rag",  # Default is 'default'
+    )
+
+    cl.user_session.set("phoenix_session", px_session)
+
+    LangChainInstrumentor().instrument()
+
+    # Create a Chroma vector store
+    embeddings = HuggingFaceEmbeddings(
+        model_name="BAAI/bge-m3",
+        # "sentence-transformers/all-mpnet-base-v2"
+    )
+
+    vector_store = Chroma(
+        collection_name=f"session_{cl.user_session.get("id")}",
+        embedding_function=embeddings,
+        persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not neccesary
+    )
+
+    # Create the LLM
+    llm = HuggingFaceEndpoint(
+        repo_id="microsoft/Phi-3-mini-4k-instruct",
+        task="text-generation",
+        max_new_tokens=512,
+        top_k=10,
+        top_p=0.95,
+        temperature=0.01,
+        do_sample=False,
+        repetition_penalty=1.03,
+    )
+
+    chat_llm = ChatHuggingFace(llm=llm, verbose=True)
+
+    urls = None
+    tracer_provider = register(
+        project_name="rag-chatbot",  # Default is 'default'
+    )
+
+    cl.user_session.set("tracer_provider", tracer_provider)
 
     # Wait for the user to upload a file
-    while files is None:
-        files = await cl.AskFileMessage(
-            content="Please upload a PDF file to begin!",
-            accept=["application/pdf"],
-            max_size_mb=20,
+    while urls is None:
+        resp = await cl.AskUserMessage(
+            content="Please send me a url of the blog you want to chat with.",
             timeout=180,
         ).send()
 
-    file = files[0]
+        print(resp)
+
+        if not await validate_url(resp["output"]):
+            continue
+        else:
+            urls = [resp["output"]]
+
+    print(urls)
+
+    url = urls[0]
 
     msg = cl.Message(
-        content=f"Processing `{file.name}`...", disable_human_feedback=True
+        content=f"Processing `{url}`..."
     )
     await msg.send()
 
     # load the file
-    texts = process_file(file)
+    texts = process_url_content(url)
 
-    print(texts[0])
+    # print(texts)
 
-    # Create a metadata for each chunk
-    metadatas = [{"source": f"{i}-pl"} for i in range(len(texts))]
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200
+    )
+    splits = text_splitter.split_documents(texts)
 
-    # # Create a Chroma vector store
-    # embeddings = OpenAIEmbeddings()
-    # docsearch = await cl.make_async(Chroma.from_texts)(
-    #     texts, embeddings, metadatas=metadatas
-    # )
+    print("Initializing Vectorstore")
+    await cl.make_async(vector_store.add_documents)(
+        documents=splits,
+    )
+    print("Initialized Vectorstore")
 
-    # message_history = ChatMessageHistory()
+    # Contextualize question #
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate one standalone question which can be understood "
+        "without the chat history. Ensure not to loose the semantic & syntactic meaning of the question."
+        "Do NOT answer the question or formulate multiple questions."
+        "just reformulate it if needed. If it can't be done return the users question as is."
+        "Follow the below JSON format of output:\n\n"
+        """{{input: <put the reformatted question here>}}"""
 
-    # memory = ConversationBufferMemory(
-    #     memory_key="chat_history",
-    #     output_key="answer",
-    #     chat_memory=message_history,
-    #     return_messages=True,
-    # )
+    )
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-    # # Create a chain that uses the Chroma vector store
-    # chain = ConversationalRetrievalChain.from_llm(
-    #     ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
-    #     chain_type="stuff",
-    #     retriever=docsearch.as_retriever(),
-    #     memory=memory,
-    #     return_source_documents=True,
-    # )
+    retriever = vector_store.as_retriever()
+    print("Initialized Retriever")
 
-    # # Let the user know that the system is ready
-    # msg.content = f"Processing `{file.name}` done. You can now ask questions!"
-    # await msg.update()
+    history_aware_retriever = create_history_aware_retriever(
+        chat_llm, retriever, contextualize_q_prompt
+    )
+    cl.user_session.set("retriever", history_aware_retriever)
+    print("Created History aware Retriever")
 
-    # cl.user_session.set("chain", chain)
+    # Answer question #
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
+    rag_chain = create_retrieval_chain(
+        history_aware_retriever, question_answer_chain
+    )
+
+    print("Created RAG Chain")
+
+    # Statefully manage chat history #
+    store = {}
+    cl.user_session.set("store", store)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+    print("Created History Aware Conversational RAG Chain")
+
+    # Let the user know that the system is ready
+    msg.content = f"Processing `{url}` done. You can now ask questions!"
+    await msg.update()
+
+    cl.user_session.set("chain", conversational_rag_chain)
+
+
+def process_chunks(chunk: dict):
+    
+    for k, v in chunk.items():
+        if isinstance(v, Document):
+            processed = {}
+            processed["metadata"] = v.metadata
+            processed["page_content"] = v.page_content
+            chunk[k] = processed
+        elif isinstance(v, list) or isinstance(v, dict) or isinstance(v, str):
+            pass
+        else:
+            chunk[k] = "Unexpected output."
+
+    return chunk
+            
 
 @cl.on_message
 async def main(message: cl.Message):
-    print("In Main Function")
-    # chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
-    # cb = cl.AsyncLangchainCallbackHandler()
+
+    runnable = cl.user_session.get("chain")
+    # sync_cb = cl.LangchainCallbackHandler()
+    cb = cl.AsyncLangchainCallbackHandler()
+    # sync_cb.on_llm_new_token(lambda token: process_chunks(token))
 
     # res = await chain.acall(message.content, callbacks=[cb])
     # answer = res["answer"]
-    # source_documents = res["source_documents"]  # type: List[Document]
 
-    # text_elements = []  # type: List[cl.Text]
+    print(f"User Session: {cl.user_session.get("id")}")
 
-    # if source_documents:
-    #     for source_idx, source_doc in enumerate(source_documents):
-    #         source_name = f"source_{source_idx}"
-    #         # Create the text element referenced in the message
-    #         text_elements.append(
-    #             cl.Text(content=source_doc.page_content, name=source_name)
-    #         )
-    #     source_names = [text_el.name for text_el in text_elements]
+    result = await runnable.ainvoke(
+        {"input": message.content},
+        config=RunnableConfig(
+            callbacks=[cb],
+            configurable={"session_id": cl.user_session.get("id")}
+        )
+    ) 
 
-    #     if source_names:
-    #         answer += f"\nSources: {', '.join(source_names)}"
-    #     else:
-    #         answer += "\nNo sources found"
+    msg = cl.Message(content=result["answer"])
+    # context_msg = cl.Message(
+    #     content=f"Context: {result["context"]}",
+    # )
 
-    await cl.Message(content=answer, elements=text_elements).send()
+    await msg.send()
+    # await context_msg.send()
+    
+@cl.on_chat_end
+async def on_chat_end():
+    # px_session = cl.user_session.get("phoenix_session")
+    df = px.Client().get_spans_dataframe()
+    persist_path = (
+        os.environ.get("TRACER_LOG_PATH")
+        + "/" + cl.user_session.get("id")
+        + ".csv"
+    )
+    directory = os.path.dirname(persist_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Created directories: {directory}")
+    else:
+        print(f"Directories already exist: {directory}")
+
+    df.to_csv(persist_path, sep=",", header=True, mode="a")
+    # px_session.stop()
+
+    # cl.user_session.clear()
+
+
